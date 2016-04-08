2625130d85246146fe4d3d19d7149ca842798473
Marx Medeiros <analista.marx@gmail.com>
2016-04-07 22:52:46 -0300

    Create Descricao.d

---
+++ Examples/LCD-16x2/pt_BR/Descricao.d
@@ -0,0 +1,22 @@
+DESCRIÇÃO:
+
+MODELO DE DISPLAY UTILIZADO:
+GVLCM1602B-13500A V1.O
+
+IDE UTILIZADA:
+ARDUINO IDE 1.6.3
+
+Placa Selecionada:
+Generic STM32F103C Series
+
+Versão 
+STM32F103C8 (20K RAM. 64K FLASH)
+
+Upload method:
+Serial
+
+Adaptador:
+USB-TTL "Profilic USB-to-Serial Comm Port (COMXX)"
+
+Chipset:
+PL2303HX
